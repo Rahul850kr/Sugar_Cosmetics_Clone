dfcc31c5bd11acea0d71fde45ac34ea5a8373735
Rahul kumar <rahul850kr@gmail.com>
2022-11-17 20:12:06 +0530

    added react-slick

--- frontend/src/Routes/Home.jsx
+++ frontend/src/Routes/Home.jsx
@@ -1,10 +1,23 @@
-import { Button } from '@chakra-ui/react'
-import React from 'react'
+import { Button } from '@chakra-ui/react';
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
+import React from 'react';
+import Slider from "react-slick";
 
 const Home = () => {
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1
+  };
+
+
   return (
     <div>
-        Welcome to homepage
+      <h1>Welcome to Home Page</h1>
     </div>
   )
 }
