d97bd5dc1ee8c083c746e1fe8a188523d6106485
Rahul kumar <rahul850kr@gmail.com>
2022-11-23 11:39:54 +0530

    logsign layout done

--- frontend/src/Routes/AllRoutes.jsx
+++ frontend/src/Routes/AllRoutes.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Home from './Home'
+import LogSign from './LogSign'
 
 const AllRoutes = () => {
   return (
     <Routes>
         <Route path='/' element={<Home/>} />
+        <Route path='/loginOrsignup' element={<LogSign/>} />
     </Routes>
   )
 }
