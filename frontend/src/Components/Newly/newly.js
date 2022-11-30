fb430d60ee715cd09d4d9be92ef287db68bc6c67
pallavee <pallaveeratnakar1714@gmail.com>
2022-11-30 23:48:49 +0530

    homepage_progree

---
+++ frontend/src/Components/Newly/newly.js
@@ -0,0 +1,55 @@
+import { Box, Image } from "@chakra-ui/react";
+import Slider from "react-slick";
+
+
+const data=[
+    {
+        images:"https://d32baadbbpueqt.cloudfront.net/Homepage/06721d4d-e610-4007-8d4d-d2fe63019695.jpg"
+    },
+    {
+        images:"https://d32baadbbpueqt.cloudfront.net/Homepage/b69d2fc2-53f5-45ed-8e89-8dcd108ed2b6.jpg"
+    },
+    {
+        images:"https://d32baadbbpueqt.cloudfront.net/Homepage/61085ebd-495f-470b-a54c-02c2569df51b.jpg"
+    },
+    {
+        images:"https://d32baadbbpueqt.cloudfront.net/Homepage/1fb4eae2-6719-4f1f-b80e-efda1cf48362.jpg"
+    },
+    {
+        images:"https://d32baadbbpueqt.cloudfront.net/Homepage/f9d36dac-9508-4dfb-b9b8-a417b308406d.jpg"
+    },
+    {
+        images:"https://d32baadbbpueqt.cloudfront.net/Homepage/a41d5b02-1299-4e1a-b4e1-ba19484796b5.gif"
+    }
+    
+
+]
+
+
+
+
+export const Newly=()=>{
+    
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1
+      };
+
+
+      return(
+        <Slider {...settings} >
+        {data.map((el)=>
+        <Box>
+            <Image src={el.images}/>
+        </Box>
+        )}
+         
+      </Slider>
+      )
+
+
+
+}
--- frontend/src/Routes/Home.jsx
+++ frontend/src/Routes/Home.jsx
@@ -1,4 +1,4 @@
-import { Box, Button, Text } from '@chakra-ui/react';
+import { Box, Button, Img, Text } from '@chakra-ui/react';
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
 import React from 'react';
@@ -6,6 +6,10 @@ import Slider from "react-slick";
 import Navbar from '../components/Navbar';
 import { SimpleSlider } from '../SimpleSlider/slider';
 import styles from "../styles/home.module.css"
+import { HomeSlider } from '../HomeSlider/sliderhome';
+import { JustSlider } from '../components/JustSlider/just';
+import { Gifting } from '../components/Gifting/gifting';
+import { Newly } from '../components/Newly/newly';
 
 const Home = () => {
 
@@ -19,10 +23,79 @@ const Home = () => {
 
  <Text className={styles.bestsell}>BESTSELLERS</Text>
 
- <Box className={styles.sli}></Box>
+ <Box className={styles.sli}>
+     <HomeSlider/>
+ </Box>
+</Box>
+
+<Text>Hot Deals</Text>
+<Box className={styles.hot}>
+
+<Box className={styles.hotdeals}>
+  <Img className={styles.hotdealsimg} src="https://d32baadbbpueqt.cloudfront.net/Homepage/d9560110-40f7-4d14-a98d-5599b45a84ef.jpg"/>
+</Box>
+<Box className={styles.hotdeals}>
+  <Img className={styles.hotdealsimg} src="https://d32baadbbpueqt.cloudfront.net/Homepage/fb82e32a-164e-432a-af89-b6593001d384.jpg"/>
+</Box>
+<Box className={styles.hotdeals}>
+  <Img className={styles.hotdealsimg} src="https://d32baadbbpueqt.cloudfront.net/Homepage/043d43d2-66eb-4d93-b725-a3d948ca3871.jpg"/>
+</Box>
+
 </Box>
 
 
+<Text>JUST IN</Text>
+<Box className={styles.slid} >
+<JustSlider/>
+</Box>
+
+
+
+
+
+  <Text>SHUKAR HAI, SUGAR HAI</Text>
+  <Box className={styles.sug}>
+<Img className={styles.sugimg} src="https://d32baadbbpueqt.cloudfront.net/Homepage/b16a42b1-3e35-42bb-b122-a517e75c490e.jpg"/>
+  </Box>
+
+
+  <Text>TOP PICKS OF THE WEEK</Text>
+<Box className={styles.hot}>
+
+<Box className={styles.hotdeals}>
+  <Img className={styles.hotdealsimg} src="https://d32baadbbpueqt.cloudfront.net/Homepage/4849cf44-c55c-4224-ba06-9dd0a5f48b67.gif"/>
+</Box>
+<Box className={styles.hotdeals}>
+  <Img className={styles.hotdealsimg} src="https://d32baadbbpueqt.cloudfront.net/Homepage/c5dc6c75-a49e-4028-b3a3-3bbbc63e49d6.jpg"/>
+</Box>
+<Box className={styles.hotdeals}>
+  <Img className={styles.hotdealsimg} src="https://d32baadbbpueqt.cloudfront.net/Homepage/9f9d2095-f019-4bb5-b961-db87fab439f6.jpg"/>
+</Box>
+
+</Box>
+
+
+
+<Text>SUPER SAVERS</Text>
+<Box className={styles.gift} >
+<Gifting/>
+</Box>
+
+
+
+
+
+
+<Text>REFER YOUR FRIENDS</Text>
+<Box className={styles.ref}>
+  <Img className={styles.refimg} src="https://d32baadbbpueqt.cloudfront.net/Homepage/8bfe8bb4-002d-4097-a773-6af33325a594.jpg"/>
+</Box>
+
+
+
+<Box>
+  <Newly/>
+</Box>
     </>
   )
 }
