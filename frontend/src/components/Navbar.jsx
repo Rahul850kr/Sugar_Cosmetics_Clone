34a3a00e5e81995ded2e02e52bccd6aeab50a275
pallavee <pallaveeratnakar1714@gmail.com>
2022-12-03 00:16:36 +0530

    navbar_dropdown_menu_progressing

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -83,9 +83,72 @@ const Navbar = () => {
 
 
 
-            <Text>Brushes</Text>
-            <Text>Skincare</Text>
-            <Text>Gifting</Text>
+           
+            <Menu className={styles.menuitem}>
+               <MenuButton as={Button}>
+               Brushes
+               </MenuButton>
+               <MenuList>
+                <Text>FACE BRUSHES</Text>
+                <MenuItem>Blend Trend Face Brush-001 Blush</MenuItem>
+                <MenuItem>Blend Trend Face Brush-003 Contour</MenuItem>
+                <MenuItem>Blend Trend Face Brush-006 Highlighter</MenuItem>
+                <MenuItem>Blend Trend Face Brush-007 Powder</MenuItem>
+                <MenuItem>Blend Trend Face Brush-052 Kabuki</MenuItem>
+                <MenuItem>Blend Trend Face Brush-075 Powder</MenuItem>
+               
+              </MenuList>
+             
+            </Menu>
+           
+            <Menu className={styles.menuitem}>
+               <MenuButton as={Button}>
+               Skincare
+               </MenuButton>
+               <MenuList>
+                
+                <MenuItem>MOISTOURIZERS</MenuItem>
+                <MenuItem>SUNSCRREN</MenuItem>
+                <MenuItem>SETTING MISTA</MenuItem>
+                <MenuItem>SHEET MASK COMBO</MenuItem>
+                <MenuItem>MASKS</MenuItem>
+                <MenuItem>COFFEE CULTURE RANGE</MenuItem>
+              </MenuList>
+             
+            </Menu>
+          
+            <Menu className={styles.menuitem}>
+               <MenuButton as={Button}>
+               Gifting
+               </MenuButton>
+               <MenuList>
+                
+                <MenuItem>SUGAR MERCH STATION</MenuItem>
+                <MenuItem>MAKEUP KITS</MenuItem>
+                <MenuItem>VALUE SETS</MenuItem>
+                <MenuItem>SUGAR SETS</MenuItem>
+                <MenuItem>BESTSELLERS</MenuItem>
+                <MenuItem>E-GIFT CARDS</MenuItem>
+              </MenuList>
+             
+            </Menu>
+            
+            <Menu className={styles.menuitem}>
+               <MenuButton as={Button}>
+               Blog
+               </MenuButton>
+               <MenuList>
+                <Text>Lips</Text>
+                <MenuItem>Lipsitics</MenuItem>
+                <MenuItem>Lip Care</MenuItem>
+                <MenuItem>Liquid Lip Colours</MenuItem>
+                <MenuItem>Value Sets</MenuItem>
+                <MenuItem>Gifting</MenuItem>
+               
+              </MenuList>
+             
+            </Menu>
+           
             <Text>Blog</Text>
             <Text>Offers</Text>
             <Text>Stores</Text>
