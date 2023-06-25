0dca12bdf4f58da11885077f75a91015abf329bb
rahulkumar <rahul850kr@gmail.com>
2023-06-25 17:50:45 +0530

    created ui for login and otp page

--- frontend/components/menubarDropdown/MenubarDropdown.tsx
+++ frontend/components/menubarDropdown/MenubarDropdown.tsx
@@ -3,41 +3,10 @@ import styles from "./MenubarDropdown.module.scss";
 import { Box, Popover, Popper, Typography } from "@mui/material";
 
 const MenubarDropdown = () => {
-  const [anchorEl, setAnchorEl] = React.useState<HTMLElement | null>(null);
-  let timeoutId: any;
-
-  const handlePopoverOpen = (event: React.MouseEvent<HTMLElement>) => {
-    clearTimeout(timeoutId);
-    setAnchorEl(event.currentTarget);
-  };
-
-  const handlePopoverClose = () => {
-    timeoutId = setTimeout(() => {
-      setAnchorEl(null);
-    }, 500);
-  };
-
-  const handleContentMouseEnter = (event: React.MouseEvent<HTMLElement>) => {
-    // Prevent closing the popover when moving cursor onto the content
-    clearTimeout(timeoutId);
-    setAnchorEl(event.currentTarget);
-  };
-
-  const handleContentMouseLeave = () => {
-    // Close the popover when moving cursor out of the content
-    handlePopoverClose;
-  };
-
-  const open = Boolean(anchorEl);
-
   return (
     <Box className={styles.menubarBox}>
       <Box className={styles.menuBox}>
-        <Box
-          onMouseEnter={handlePopoverOpen}
-          onMouseLeave={handlePopoverClose}
-          className={styles.eachMenuBox}
-        >
+        <Box className={styles.eachMenuBox}>
           <p>LIPS</p>
         </Box>
         <Box className={styles.eachMenuBox}>
@@ -74,40 +43,6 @@ const MenubarDropdown = () => {
           <p>STORES</p>
         </Box>
       </Box>
-      {/* <Popper open={open} anchorEl={anchorEl}>
-        <Box sx={{ border: 1, p: 1, bgcolor: "background.paper" }}>
-          The content of the Popper.
-        </Box>
-      </Popper> */}
-      <Popover
-        id="mouse-over-popover"
-        sx={{
-          pointerEvents: "none",
-        }}
-        open={open}
-        anchorEl={anchorEl}
-        anchorOrigin={{
-          vertical: "bottom",
-          horizontal: "left",
-        }}
-        transformOrigin={{
-          vertical: "top",
-          horizontal: "left",
-        }}
-        disableRestoreFocus
-        onClose={handlePopoverClose}
-      >
-        <Box
-          onMouseEnter={handleContentMouseEnter}
-          onMouseLeave={handleContentMouseLeave}
-          sx={{ p: 1, cursor: "pointer" }}
-          onClick={() => {
-            console.log("Hello Clicked");
-          }}
-        >
-          I use Popover.
-        </Box>
-      </Popover>
     </Box>
   );
 };
