39bf563df2b84c7e61db1a2cb05128e46badbe81
rahulkumar <rahul850kr@gmail.com>
2023-07-10 11:33:42 +0530

    added category section

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -4,26 +4,26 @@ import styles from "./page.module.scss";
 import Carousel from "@/components/carousel/Carousel";
 import { Box, Paper } from "@mui/material";
 import BottomBar from "@/components/bottom-bar/BottomBar";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
-  // const router = useRouter();
+  const router = useRouter();
 
-  // const handleNavigateBottomBar = (newValue: any) => {
-  //   if (newValue == 0) {
-  //     router.push("/");
-  //   } else if (newValue == 1) {
-  //     router.push("/signup");
-  //   } else {
-  //     router.push("/login");
-  //   }
-  // };
+  const handleNavigateBottomBar = (newValue: any) => {
+    if (newValue == 0) {
+      router.push("/");
+    } else if (newValue == 1) {
+      router.push("/categories");
+    } else {
+      router.push("/login");
+    }
+  };
 
   return (
     <Box className={styles.homeContainer}>
       <Navbar />
       <Carousel />
-      {/* <BottomBar handleNavigateBottomBar={handleNavigateBottomBar} /> */}
+      <BottomBar handleNavigateBottomBar={handleNavigateBottomBar} />
     </Box>
   );
 }
